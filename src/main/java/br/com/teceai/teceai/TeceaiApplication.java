fae2cba0905671c8da6490572eab07368518f0b3
Fillipe <incarnialol2@gmail.com>
2025-11-16 19:20:31 -0300

    initial commit

---
+++ src/main/java/br/com/teceai/teceai/TeceaiApplication.java
@@ -0,0 +1,13 @@
+package br.com.teceai.teceai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeceaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeceaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/teceai/teceai/TeceaiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teceai.teceai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeceaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
